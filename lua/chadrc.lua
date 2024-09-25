de2a4f7342e9048dcb531a02932ae05948fd125b
rock-tran <rock.tran@manadr.com>
2024-09-25 23:55:14 +0700

    Fix conform configs; Update mason pkgs

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,17 +1,77 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
   theme = "monekai",
-
   -- hl_override = {
   -- 	Comment = { italic = true },
   -- 	["@comment"] = { italic = true },
   -- },
+}
+
+M.mason = {
+  pkgs = {
+    "lua-language-server",
+    "stylua",
+
+    -- web dev
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    -- "tsserver",
+    "deno",
+    "prettier",
+    -- "emmet-ls",
+    "json-lsp",
+    -- "tailwindcss-language-server",
+    "unocss-language-server",
+    -- "emmet-language-server",
+
+    -- shell
+    "shfmt",
+    "shellcheck",
+    "bash-language-server",
+
+    -- "clangd",
+    -- "clang-format",
+    -- xml
+    "lemminx",
+    "xmlformatter",
+
+    -- ruby
+    -- "rubocop",
+    -- "solargraph",
+
+    -- python
+    "pyright",
+    "autopep8",
+    "debugpy",
+    -- markdown
+    "marksman",
+    -- yaml
+    "yaml-language-server",
+    "yamlfmt",
+    "yamllint",
+    -- kotlin
+    -- "kotlin-language-server"
+    "ktlint",
+    "terraform-ls",
+    -- java
+    "google-java-format",
+    -- rust
+    "rust-analyzer",
+    -- cmake
+    "cmake-language-server",
+    -- make file
+    -- "autotools-language-server",
+    "texlab",
+    "latexindent",
+    "gopls",
+  },
 }
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,10 @@ local M = {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    -- opts = require "configs.conform",
+    config = function()
+	    require "configs.conform"
+    end
   },
   {
     "neovim/nvim-lspconfig",
