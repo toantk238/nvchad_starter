0514c9d3298c6d05620c1ac9a8d1466892d04119
rock-tran <rock.tran@manadr.com>
2025-05-04 23:45:05 +0700

    Fix a bit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -287,6 +287,7 @@ local M = {
   {
     "HiPhish/rainbow-delimiters.nvim",
     lazy = false,
+    submodules = false,
     branch = "master",
   },
   {
@@ -416,55 +417,55 @@ local M = {
   },
 }
 
+package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;"
+
 local optionalPlugins = {
   {
     "3rd/image.nvim",
+    -- enabled = true,
     lazy = false,
-    config = function()
-      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;"
-      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;"
-      require("image").setup {
-        backend = "kitty",
-        kitty_method = "normal",
-        processor = "magick_rock", -- or "magick_cli"
-        integrations = {
-          markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            floating_windows = false, -- if true, images will be rendered in floating markdown windows
-            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-          },
-          neorg = {
-            enabled = true,
-            filetypes = { "norg" },
-          },
-          typst = {
-            enabled = true,
-            filetypes = { "typst" },
-          },
-          html = {
-            enabled = false,
-          },
-          css = {
-            enabled = false,
-          },
+    tag = "v1.3.0",
+    otps = {
+      backend = "kitty",
+      kitty_method = "normal",
+      processor = "magick_rock", -- or "magick_cli"
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          floating_windows = false, -- if true, images will be rendered in floating markdown windows
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
-        tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
-      }
-    end,
+        neorg = {
+          enabled = true,
+          filetypes = { "norg" },
+        },
+        typst = {
+          enabled = true,
+          filetypes = { "typst" },
+        },
+        html = {
+          enabled = false,
+        },
+        css = {
+          enabled = false,
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+      tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+    },
     cond = function()
-      local hasLuaRocks = vim.fn.isdirectory(vim.fn.expand "$HOME/.luarocks")
-      return hasLuaRocks and notInsideKittyScrollback
+      return true
     end,
   },
   {
@@ -488,10 +489,6 @@ local optionalPlugins = {
     cond = function()
       return vim.fn.executable "kubectl" == 1
     end,
-  },
-  {
-    "luarocks/hererocks",
-    lazy = true,
   },
 }
 
