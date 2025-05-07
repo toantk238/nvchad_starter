200cab9fe4d33acd9ca22348164a3e3eb0fcdf8b
rock-tran <rock.tran@manadr.com>
2025-05-07 23:14:16 +0700

    Fix image.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -417,14 +417,10 @@ local M = {
   },
 }
 
-package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;"
-package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;"
-
 local optionalPlugins = {
   {
     "3rd/image.nvim",
-    -- enabled = true,
-    lazy = false,
+    event = "VeryLazy",
     tag = "v1.3.0",
     otps = {
       backend = "kitty",
@@ -436,6 +432,7 @@ local optionalPlugins = {
           clear_in_insert_mode = false,
           download_remote_images = true,
           only_render_image_at_cursor = false,
+          only_render_image_at_cursor_mode = "popup",
           floating_windows = false, -- if true, images will be rendered in floating markdown windows
           filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         },
@@ -459,13 +456,19 @@ local optionalPlugins = {
       max_width_window_percentage = nil,
       max_height_window_percentage = 50,
       window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-      editor_only_render_when_focused = true, -- auto show/hide images when the editor gains/looses focus
+      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
+      editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
       tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
       hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
     },
+    config = function(_, opts)
+      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua"
+      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua"
+      require("image").setup(opts)
+    end,
     cond = function()
-      return true
+      local luarocks = vim.fn.expand("$HOME" .. "/.luarocks")
+      return vim.fn.isdirectory(luarocks) == 1
     end,
   },
   {
