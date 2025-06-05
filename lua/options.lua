841d9550821d0b4c98c40c1a509fca4cab109733
rock-tran <rock.tran@manadr.com>
2025-06-05 10:38:35 +0700

    Check if lemonade is executable

--- lua/options.lua
+++ lua/options.lua
@@ -4,17 +4,21 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
-vim.g.clipboard = {
-  name = "lemonade",
-  copy = {
-    ["+"] = { "lemonade", "copy", "--host=127.0.0.1" },
-    ["*"] = { "lemonade", "copy", "--host=127.0.0.1" },
-  },
-  paste = {
-    ["+"] = { "lemonade", "paste", "--host=127.0.0.1" },
-    ["*"] = { "lemonade", "paste", "--host=127.0.0.1" },
-  },
-  cache_enabled = false,
-}
+-- Verify if lemonade is installed
+
+if vim.fn.executable "lemonade" then
+  vim.g.clipboard = {
+    name = "lemonade",
+    copy = {
+      ["+"] = { "lemonade", "copy", "--host=127.0.0.1" },
+      ["*"] = { "lemonade", "copy", "--host=127.0.0.1" },
+    },
+    paste = {
+      ["+"] = { "lemonade", "paste", "--host=127.0.0.1" },
+      ["*"] = { "lemonade", "paste", "--host=127.0.0.1" },
+    },
+    cache_enabled = false,
+  }
+end
 
 vim.g.wordmotion_prefix = "<Leader>"
