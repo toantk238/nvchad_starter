f59334d9e952bcc35c76d94a98d176ab8412793e
rock-tran <rock.tran@manadr.com>
2026-06-10 13:42:59 +0700

    Use plugin to setup yamlls

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -686,6 +686,21 @@ local optionalPlugins = {
       }
     end,
   },
+  {
+    "mosheavni/yaml-companion.nvim",
+    opts = {
+      -- Add any options here, or leave empty to use the default settings
+      -- lspconfig = {
+      --   settings = { ... }
+      -- },
+    },
+    config = function(_, opts)
+      local cfg = require("yaml-companion").setup(opts)
+      vim.lsp.config("yamlls", cfg)
+      vim.lsp.enable "yamlls"
+    end,
+    lazy = false,
+  },
 }
 
 local avante = {
